5aa5370f816c59dc3783ef13a0f4b51508513c34
Alejandro Servin <alejandro.servin@upa.edu.py>
2019-10-01 08:34:47 -0400

    segunda subida

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import React from 'react';
 import './App.css';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
+ // import List from '@material-ui/core/List';
+ // import ListItem from '@material-ui/core/ListItem';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+// import axios from 'axios';
+// import Personas from './Servicios.js';
 
-import axios from 'axios';
+// import PropTypes from 'prop-types';
 
-import Personas from './Servicios.js';
+// class Links extends React.Component{
+//   render(){
+//     return(
+//       <div>
+//         <nav>
+//           <ul>
+//             <li> <Link to="/">Home</Link></li>
+//             <li> <Link to="/about">About</Link></li>
+//             <li> <Link to="/users">Users</Link></li>
+//             <li> <Link to="/topics">Topics</Link></li>
+//           </ul>
+//         </nav>
+//       </div>
+//     );
+//   }
+// }
+
+// class Rutas extends React.Component{
+//   render(){
+//     return(
+//       <>
+//         <Route path="/" exact component={Index} />
+//         <Route path="/about" component={About} />
+//         <Route path="/users" component={Users} />
+//         <Route path="/topics" component={Topics} />
+//       </>
+//     );
+//   }
+// }
 
 function Index(){
-  return <> <h1>Home</h1> <Personas /> </>
+  return(
+          <>
+          <center>
+            <h1>Bienvenido/a a ToDo List.</h1>
+            <h2>Aquí encontrarás todos tus recordarios y alarmas para organizar
+            mejor tu día a día.</h2>
+          </center>
+          </>
+        );
 }
 
 function About(){
-  return <> <h1>About</h1></>
+  return <> <center><h2>Esta es una aplicación creada en React. La aplicación
+  ToDo List te permite anotar todas tus tareas por hacer, así como
+  crear recordatorios y alarmas. </h2></center></>
 }
 
-function Users(){
-  return <> <h1>Users</h1></>
+function Lists(){
+  return <> <h1>Lista</h1></>
 }
 
 function Topics({ match }) {
@@ -52,42 +92,72 @@ function Topics({ match }) {
 
 function Topic({ match }) {
   // return <h3>Requested topic ID: {match.params.topicId}</h3>;
-  return <h3>Este es un tópico</h3>
+  return <h3>Este es un tópico </h3>
 
 }
 
 
+class Seccion extends React.Component{
+  render(){
+    const { style } = this.props;
+    return(
+          // <div style={{float: 'left', width: '20%', border: '3px solid black'}}>
+          <div style={style}>
+            {this.props.contenido}
+          </div>
 
-class App extends React.Component{
-
-  render() {
-    return (
-
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li> <Link to="/">Home</Link></li>
-              <li> <Link to="/about/">About</Link></li>
-              <li> <Link to="/users/">Users</Link></li>
-              <li> <Link to="/topics">Topics</Link></li>
-            </ul>
-          </nav>
-
-          <Route path="/" exact component={Index} />
-          <Route path="/about/" component={About} />
-          <Route path="/users/" component={Users} />
-          <Route path="/topics" component={Topics} />
-        </div>
-      </Router>
-
-
-
-      // se muestra el componente Personas, cuyos datos fueron extraídos de una api (servicio)
-      //<Personas />
-
-    )
+    );
   }
 }
+
+class App extends React.Component{
+  render() {
+    /* contenido del sidebar, que son los links */
+    const styleLi = {margin:'5px'};
+    const contenidoSidebar =
+      <nav>
+      <center>
+        <ul style={{listStyle: 'none'}}>
+          <li style={styleLi}> <Link to="/"><h1>Home</h1></Link></li>
+          <li style={styleLi}> <Link to="/list"><h1>List</h1></Link></li>
+          <li style={styleLi}> <Link to="/about"><h1>About</h1></Link></li>
+          <li style={styleLi}> <Link to="/topics"><h1>Topics</h1></Link></li>
+        </ul>
+      </center>
+      </nav>     ;
+
+    /* contenido de la seccion principal, que son las rutas */
+    const contenidoPrincipal =
+    <div>
+      <Route path="/" exact component={Index} />
+      <Route path="/about" component={About} />
+      <Route path="/list" component={Lists} />
+      <Route path="/topics" component={Topics} />
+    </div>;
+
+    return (
+      <>
+      <header style={{width: '99%', backgroundColor: 'blue', padding: '4px'
+      , color: 'white'}}>
+        <center>
+          <h1>ToDo List</h1>
+        </center>
+      </header>
+
+      <Router>
+        <Seccion name="sidebar" style={{float: 'left', width: '20%',
+        height:'500px', background: 'lightblue', marginTop: '1%'
+        , marginRight: '1%'}} contenido={contenidoSidebar} />
+
+        <Seccion name="seccionPrincipal" style={{float: 'left', width: '77%'
+        , height:'500px', background: 'lightblue', marginTop: '10px'
+        , marginRight: '1%',paddingLeft:'10px'}}
+        contenido={contenidoPrincipal} />
+      </Router>
+    </>
+    )
+  }
+};
+
 
 export default App;
